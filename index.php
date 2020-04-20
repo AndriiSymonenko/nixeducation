45b4976333c7f7548070b984f1777ef6ec4bb9f2
Andrii Symonenko <andrii.symonenko.dev@gmail.com>
2020-04-20 21:39:01 +0300

    add lesson-one

--- index.php
+++ index.php
@@ -1,31 +1,54 @@
+
 <?php
 
+// with while
+//$i = 1;
+//echo "<table border = \"1\">\n";
+//$firstNumber = 1;
+//while ($i <= 2) {
+//    echo "<tr>";
+//    $n = 1;
+//
+//    while ($n <= 5) {
+//        echo "<td>";
+//
+//        $secondNumber = 1;
+//
+//        while ($secondNumber <= 10) {
+//            echo "$firstNumber X $secondNumber" . "=" . $firstNumber * $secondNumber . "<br>";
+//            $secondNumber++;
+//        }
+//        echo "</td>";
+//        $n++;
+//        $firstNumber++;
+//    }
+//    echo "</tr>";
+//    $i++;
+//
+//}
+//
+//echo "</table>";
+//whith for
 
+echo "<link rel='stylesheet' href='style.css'>";
 
-$i = 1;
-echo "<table border = \"1\">\n";
+echo "<table class='multiplication-table' border = \"1\">\n";
+for ($firstNumber = 1; $firstNumber <= 10; $firstNumber++) {
+    for ($tr = 1; $tr <= 2; $tr++ ) {
+        echo "<tr>";
+        for ($td = 1; $td <= 5; $td++ ) {
+            echo "<td>";
 
-while ($i <= 2) {
-    echo "<tr>";
-    $n = 1;
-    while ($n <= 5) {
-        echo "<td>";
-        
-    $firstNumber = 1;
-    $secondNumber = 1;
-
-while ($secondNumber <= 10) {
-    echo "$firstNumber X $secondNumber" . "=" . $firstNumber * $secondNumber . "<br>";
-    $secondNumber++;
+            for ($secondNumber = 1; $secondNumber <= 10; $secondNumber++) {
+                echo "$firstNumber X $secondNumber" . " " . "=" .  " " . $firstNumber * $secondNumber . "<br>";
+            }
+            echo "</td>";
+            $firstNumber++;
+        }
+        echo "</tr>";
     }
-        echo "</td>";
-        $n++;
-    }
-    echo "</tr>";
-    $i++;
 }
-
 echo "</table>";
 
-
 ?>
+
